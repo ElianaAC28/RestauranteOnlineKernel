1a272656f4d0bee3611cdd1b22591aa721c0d190
Mezcly08 <joiroce@unicauca.edu.co>
2021-09-10 12:29:48 -0500

    pruebas unitarias almuerzo

--- Restaurante-core/src/test/java/co/edu/unicauca/microkernel/restaurante/core/services/AlmuerzoServiceTest.java
+++ Restaurante-core/src/test/java/co/edu/unicauca/microkernel/restaurante/core/services/AlmuerzoServiceTest.java
@@ -77,81 +77,99 @@ public class AlmuerzoServiceTest {
     /**
      * Test of updateAlmuerzo method, of class AlmuerzoService.
      */
-//    @Test
-//    public void testUpdateAlmuerzo() {
-//        System.out.println("updateAlmuerzo");
-//        Almuerzo parAlmuerzo = null;
-//        parAlmuerzo.setIdAlmuerzo("122");
-//        
-//        
-//        IAlmuerzoRepository repo = Factory.getInstance().getRepositoryAlmuerzo();
-//        AlmuerzoService instance = new AlmuerzoService(repo);
-//        String expResult = "";
-//        String result = instance.updateAlmuerzo(parAlmuerzo);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testUpdateAlmuerzo() {
+        
+        System.out.println("updateAlmuerzo");
+        Almuerzo parAlmuerzo = new Almuerzo();
+        parAlmuerzo.setIdAlmuerzo("1");
+        parAlmuerzo.setCostoAlm("18"); //utilizamos para enviar el id del componente nuevo
+        parAlmuerzo.setRestId("4"); // utilizamos para enviar el id del componente viejo
+        
+        
+        
+        IAlmuerzoRepository repo = Factory.getInstance().getRepositoryAlmuerzo();
+        AlmuerzoService instance = new AlmuerzoService(repo);
+        String expResult = "1";
+        String result = instance.updateAlmuerzo(parAlmuerzo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
 
     /**
      * Test of updateCosto method, of class AlmuerzoService.
      */
-//    @Test
-//    public void testUpdateCosto() {
-//        System.out.println("updateCosto");
-//        Almuerzo parAlmuerzo = null;
-//        AlmuerzoService instance = new AlmuerzoService();
-//        String expResult = "";
-//        String result = instance.updateCosto(parAlmuerzo);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+        @Test
+        public void testUpdateCosto() {
+            System.out.println("updateCosto");
+            Almuerzo parAlmuerzo = new Almuerzo();
+            parAlmuerzo.setIdAlmuerzo("122");
+            parAlmuerzo.setCostoAlm("50000"); 
+            parAlmuerzo.setRestId("1"); //
+            
+            IAlmuerzoRepository repo = Factory.getInstance().getRepositoryAlmuerzo();
+            AlmuerzoService instance = new AlmuerzoService(repo);
+            String expResult = "122";
+            String result = instance.updateCosto(parAlmuerzo);
+            assertEquals(expResult, result);
+            // TODO review the generated test code and remove the default call to fail.
+           // fail("The test case is a prototype.");
+        }
 
     /**
      * Test of deleteCompAlmuerzo method, of class AlmuerzoService.
      */
-//    @Test
-//    public void testDeleteCompAlmuerzo() {
-//        System.out.println("deleteCompAlmuerzo");
-//        Almuerzo parAlmuerzo = null;
-//        AlmuerzoService instance = new AlmuerzoService();
-//        String expResult = "";
-//        String result = instance.deleteCompAlmuerzo(parAlmuerzo);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testDeleteCompAlmuerzo() {
+        System.out.println("deleteCompAlmuerzo");
+        Almuerzo parAlmuerzo = new Almuerzo();
+        parAlmuerzo.setIdAlmuerzo("1");
+        parAlmuerzo.setRestId("1"); // id del componente
+
+        IAlmuerzoRepository repo = Factory.getInstance().getRepositoryAlmuerzo();
+        AlmuerzoService instance = new AlmuerzoService(repo);
+        String expResult = "1";
+        String result = instance.deleteCompAlmuerzo(parAlmuerzo);
+        assertEquals(expResult, result);
+    }
 
     /**
      * Test of contarAlmu method, of class AlmuerzoService.
      */
-//    @Test
-//    public void testContarAlmu() {
-//        System.out.println("contarAlmu");
-//        Almuerzo parAlmuerzo = null;
-//        AlmuerzoService instance = new AlmuerzoService();
-//        String expResult = "";
-//        String result = instance.contarAlmu(parAlmuerzo);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testContarAlmu() {
+        System.out.println("contarAlmu");
+        Almuerzo parAlmuerzo = new Almuerzo();
+        parAlmuerzo.setIdAlmuerzo("133");
+        parAlmuerzo.setRestId("1"); // id del componente
+
+        IAlmuerzoRepository repo = Factory.getInstance().getRepositoryAlmuerzo();
+        AlmuerzoService instance = new AlmuerzoService(repo);
+        String expResult = "0";
+        String result = instance.contarAlmu(parAlmuerzo);
+        
+        assertEquals(expResult, result);
+        
+
+    }
 
     /**
      * Test of asociarComp method, of class AlmuerzoService.
      */
-//    @Test
-//    public void testAsociarComp() {
-//        System.out.println("asociarComp");
-//        String almuid = "";
-//        String compid = "";
-//        AlmuerzoService instance = new AlmuerzoService();
-//        String expResult = "";
-//        String result = instance.asociarComp(almuid, compid);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testAsociarComp() {
+        System.out.println("asociarComp");
+        String almuid = "122"; // id almuerzo
+        String compid = "3"; // id componente asociar
+        IAlmuerzoRepository repo = Factory.getInstance().getRepositoryAlmuerzo();
+        AlmuerzoService instance = new AlmuerzoService(repo);
+        String expResult = "122";
+        String result = instance.asociarComp(almuid, compid);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+        // Dato erroneo
+    }
 
 }
